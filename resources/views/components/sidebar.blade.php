31cdd122d91881332cb6ee87ecbac9ab9ab52c50
Ronish Peskar <ronishpeskar123@gmail.com>
2025-01-06 22:40:30 +0545

    initial comit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html"> <img alt="image" src="/assets/img/logo.png" class="header-logo" /> <span
+            class="logo-name">Admin</span>
+        </a>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+          <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+      </ul>
+      <li class="dropdown">
+        <a href="index.html" class="nav-link"><i data-feather="home"></i><span>Company</span></a>
+    </ul>
+    <li class="dropdown">
+        <a href="index.html" class="nav-link"><i data-feather="grid"></i><span>Category</span></a>
+    </ul>
+    <li class="dropdown">
+        <a href="index.html" class="nav-link"><i data-feather="tv"></i><span>Post</span></a>
+    </ul>
+    </aside>
+  </div>
